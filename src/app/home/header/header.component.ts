40d0f5fa3ec6508b9d6c6346ba0e17de8122b80e
isaacjiang <isaacjiang.ca@gmail.com>
2019-04-18 23:29:49 -0400

    2019-04-18 21:10-23:40

---
+++ src/app/home/header/header.component.ts
@@ -0,0 +1,222 @@
+/**
+ * Created by isaacjiang on 2017-09-01.
+ */
+import {Component} from '@angular/core';
+import {AlertController, Events} from '@ionic/angular';
+
+
+@Component({
+    selector: 'edp-header',
+    templateUrl: 'header.component.html'
+})
+
+
+export class HeaderComponent {
+    public current_user: any = {username: ''};
+    // public user_info:any;
+    public viewCtl = {showJoinTeam: true, showSignup: true, showLogOut: true};
+
+
+    constructor(public alertController: AlertController, public events: Events) {
+    }
+
+//     eventsHandles(root) {
+//       root.events.subscribe('root-update-user-status', (user) => {
+//         this.current_user=user
+//         this.updateViewCtrl(user)
+//       })
+//       root.events.subscribe('root-login-success', (user) => {
+//         this.current_user=user
+//         this.updateViewCtrl(user)
+//       })
+//       root.events.subscribe('root-update-user-info', (user_info) => {
+//         this.user_info=user_info
+//         if(user_info.userInfo.status =='Init'){this.viewCtl.showJoinTeam=true}
+//       })
+//
+//     }
+//
+//     updateViewCtrl(current_user){
+//       if(current_user['status']){
+//         console.log(current_user)
+//         if(current_user['status']['is_authenticated']){
+//           this.viewCtl.showLogOut=true
+//           if(current_user['permission']==0){ this.viewCtl.showSignup=true}
+//         }
+//         else if(current_user['status']['is_anonymous']){
+//           this.viewCtl.showLogOut=false
+//           this.viewCtl.showSignup=false
+//         }
+//       }
+//     }
+//
+    loadPage(pageName) {
+        // this.events.publish('header-load-page',pageName)
+    }
+
+    toggleMenu(menuId) {
+        // this.events.publish('header-toggle-menu',menuId)
+    }
+
+    async joinTeam() {
+        const alert = await this.alertController.create({
+            header: 'SignUp',
+            // message: 'Enter a name for this new album you're so keen on adding',
+            inputs: [
+                {
+                    type: 'radio',
+                    label: 'Team A',
+                    value: 'Team A'
+                },
+                {
+                    type: 'radio',
+                    label: 'Team B',
+                    value: 'Team B'
+                },
+                {
+                    type: 'radio',
+                    label: 'Team C',
+                    value: 'Team C'
+                },
+                {
+                    type: 'radio',
+                    label: 'Team D',
+                    value: 'Team D'
+                },
+                {
+                    type: 'radio',
+                    label: 'Team E',
+                    value: 'Team E'
+                },
+                {
+                    type: 'radio',
+                    label: 'Team F',
+                    value: 'Team F'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Join LegacyCo',
+                    handler: data => {
+                        console.log('Join LegacyCo' + data);
+
+
+                        // this.api.post('/api/dtools/jointeam',{
+                        //   username: this.current_user.username,
+                        //   //taskID:this.task_info.taskID,
+                        //   companyName :'LegacyCo',
+                        //   teamName : data,
+                        //   //period:this.task_info.period,
+                        //   data:{username:this.current_user.username,
+                        //     teamName:data,
+                        //     companyName: 'LegacyCo',
+                        //     userrole: 'CEO'}
+                        // }).subscribe(resp=>{
+                        //    console.log(resp)
+                        //   this.events.publish('header-load-page','home')
+                        //   this.updateViewCtrl(this.current_user)
+                        // })
+
+
+                    }
+                },
+                {
+                    text: 'Join NewCo',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                        // this.api.post('/api/dtools/jointeam',{
+                        //   username: this.current_user.username,
+                        //   //taskID:this.task_info.taskID,
+                        //   companyName :'LegacyCo',
+                        //   teamName : data,
+                        //   //period:this.task_info.period,
+                        //   data:{username:this.current_user.username,
+                        //     teamName:data,
+                        //     companyName: 'NewCo',
+                        //     userrole: 'CEO'}
+                        // }).subscribe(resp=>{
+                        //   console.log(resp)
+                        //   this.events.publish('header-load-page','home')
+                        //   this.updateViewCtrl(this.current_user)
+                        // })
+                    }
+                }
+            ]
+        });
+
+
+        await alert.present();
+
+    }
+
+    logout() {
+        // this.events.publish('header-logout-current-user')
+    }
+
+    async signup() {
+        const prompt = await this.alertController.create({
+            header: 'SignUp',
+            // message: 'Enter a name for this new album you're so keen on adding',
+            inputs: [
+                {
+                    name: 'username',
+                    placeholder: 'Username'
+                },
+                {
+                    name: 'email',
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                {
+                    name: 'password',
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                {
+                    name: 'password2',
+                    type: 'password',
+                    placeholder: 'Confirm Password'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Regular User',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                        data.permission = 1;
+                        if (data.username !== '' && data.password !== '' && data.username.length >= 6 && data.password === data.password2) {
+                            this.events.publish('signup-do-signup', data);
+                        } else {
+// todo send message
+                        }
+                    }
+                }
+            ]
+        });
+
+//         if (this.current_user.permission === 0) {
+//             prompt.addButton({
+//                 text: 'Administrator',
+//                 handler: data => {
+//                     console.log('Saved clicked', data);
+//                     data.permission = 0;
+//                     if (data.username !== '' && data.password !== '' && data.username.length >= 6 && data.password === data.password2) {
+//                         this.events.publish('signup-do-signup', data);
+//                     } else {
+// // todo send message
+//                     }
+//
+//                 }
+//             });
+//         }
+        await prompt.present();
+    }
+
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {MainMenuDirective} from '../directives/main.menu.directive';
+import {HelpMenuDirective} from '../directives/help.menu.directive';
+import {BudgetMenuDirective} from '../directives/budget.menu.directive';
+import {FixedMenuDirective} from '../directives/fixed.menu.directive';
+import {ContentDirective} from '../directives/content.directive';
+import {StatusDirective} from '../directives/status.directive';
+import {Events, LoadingController, MenuController, ModalController, NavController, ToastController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +14,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(MainMenuDirective) mainMenuHost: MainMenuDirective;
+  @ViewChild(HelpMenuDirective) helpMenuHost: HelpMenuDirective;
+  @ViewChild(BudgetMenuDirective) budgetMenuHost: BudgetMenuDirective;
+  @ViewChild(FixedMenuDirective) fixedMenuHost: FixedMenuDirective;
+  @ViewChild(ContentDirective) contentHost: ContentDirective;
+  @ViewChild(StatusDirective) statusMenuHost: StatusDirective;
+
+  public current_user: any;
+  public user_info: any;
+  private loader: any;
+  constructor(
+      public events: Events,
+      public menuCtrl: MenuController,
+      public navCtrl: NavController,
+      public modalCtl: ModalController,
+      public toastCtrl: ToastController,
+      public loadingCtrl: LoadingController,
+      private componentFactoryResolver: ComponentFactoryResolver) {
+
+  }
+
+  ionViewWillEnter() {
+    // this.loader = this.loadingCtrl.create({
+    //   content: 'Please wait...',
+    //   duration: 2000
+    // });
+    // this.loader.present();
+  }
+
+  ionViewDidEnter() {
+  }
+
+  ionViewWillLeave() {
+  }
+
 
 }
