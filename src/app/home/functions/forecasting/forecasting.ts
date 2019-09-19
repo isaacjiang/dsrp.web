5467c6b1dc070078b668a834169d27eb1d78c25c
jake <jakechen0816@gmail.com>
2019-09-18 20:53:13 -0400

    20190918

--- src/app/home/functions/forecasting/forecasting.ts
+++ src/app/home/functions/forecasting/forecasting.ts
@@ -13,7 +13,7 @@ export class Forecasting implements OnInit {
     private task_info: any;
     private tabs: any;
     private parameters: any = {tabs_value: ['projected_sale'], tabs_disp: ['Projected Sale'], labels: ['B2B', 'B2C', 'New Offering']};
-    private forcast: any;
+    private forecast: any;
 
     constructor(private events: Events,
                 private shareService: ShareService,
@@ -33,7 +33,7 @@ export class Forecasting implements OnInit {
 
     private initialization() {
         this.tabs = this.parameters.tabs_value[0];
-        this.forcast = {b2b: 0, b2c: 0, newoffering: 0, total: 0, total_disp: '0'};
+        this.forecast = {b2b: 0, b2c: 0, newoffering: 0, total: 0, total_disp: '0'};
         if (this.task_info.companyName === 'NewCo') {
             this.parameters.labels = ['Niche #1', 'Niche #2', 'Niche #3', 'Total'];
         }
@@ -41,21 +41,21 @@ export class Forecasting implements OnInit {
         this.httpService.get('/api/forecasting/' + this.shareService.current_user.companyId)
             .subscribe((forecast) => {
                     if (Object.keys(forecast).length > 0) {
-                        this.forcast = forecast;
-                        this.forcast.total_disp = this.shareService.formatNum(this.forcast.total);
+                        this.forecast = forecast;
+                        this.forecast.total_disp = this.shareService.formatNum(this.forecast.total);
                     }
                 }
             );
     }
 
     private onChange() {
-        this.forcast.total = this.forcast.b2b + this.forcast.b2c + this.forcast.newoffering;
-        this.forcast.total_disp = this.shareService.formatNum(this.forcast.total);
+        this.forecast.total = this.forecast.b2b + this.forecast.b2c + this.forecast.newoffering;
+        this.forecast.total_disp = this.shareService.formatNum(this.forecast.total);
     }
 
     private submit() {
         // console.log(this.forcast)
-        this.httpService.post('/api/forecasting/save', this.forcast
+        this.httpService.post('/api/forecasting/save', this.forecast
         ).subscribe(resp => {
             console.log(resp);
             this.dismiss();
@@ -63,7 +63,7 @@ export class Forecasting implements OnInit {
 
     }
 
-    private openPdf() {
+    private openPdf(event) {
         // this.modalCtl.create(PdfViewerComponent, fileInfo).present();
     }
 }
